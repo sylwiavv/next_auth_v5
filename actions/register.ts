01debf94c08d849b1a47811f9037c5033d09b7cb
sylwia <sylwiavv@gmail.com>
2024-08-27 10:56:16 +0200

    Add server action to register and create user in db

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,41 @@
-"use client"
+"use server"
 
-import {RegisterSchema} from "@/schemas";
 import * as z from "zod";
+import {db} from "@/lib/db";
+import bcrypt from "bcrypt";
+import {RegisterSchema} from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-const validatedSchema = RegisterSchema.safeParse(values)
+    const validatedSchema = RegisterSchema.safeParse(values)
 
     if (!validatedSchema.success) {
         return {error: "Invalid fields!"}
     }
+
+    const { password, email, name } = validatedSchema.data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword,"hashedPassword");
+
+    const existingUser = await db.user.findUnique({
+        where:{
+            email
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email is already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    //TODO: send verification emil token
 
     return {success: "Email sent"}
 }
